000bbc893eaee6a5205e57433e7dcaa910796e28
dilshad-ferdousi <dilshad.tonu@gmail.com>
2022-03-22 07:55:41 +0600

    use map, reduce and filter

--- Practice_JS_array.js
+++ Practice_JS_array.js
@@ -25,3 +25,45 @@ for(i=0; i<marks.length;i++)
     
 }
 console.log("Summetion of the array: "+sum)
+
+console.log("****************")
+// New type of method 'reduce' whch is a replace of for loop
+let total = marks.reduce((sum,mark) =>(sum+mark),0)
+console.log(total)
+
+console.log("****************")
+let oddEvenArrray = [11,12,13,14,15,16,17]
+console.log(oddEvenArrray)
+
+let evenArray = []
+let oddArray = []
+for (i=0; i<oddEvenArrray.length; i++)
+{
+    if (oddEvenArrray[i]%2 ==0)
+    {
+        evenArray.push(oddEvenArrray[i])
+        
+    }
+    else
+    {
+        oddArray.push(oddEvenArrray[i])
+        
+    }
+}
+console.log(evenArray)
+console.log(oddArray)
+
+/////// use Filter method to replace for loop
+console.log("****************")
+let useFilterMethodToGetEven = (oddEvenArrray.filter(oddEvenArrray=> oddEvenArrray%2==0))
+console.log(useFilterMethodToGetEven)
+
+let useFilterMethodToGetOdd =(oddEvenArrray.filter(oddEvenArrray => oddEvenArrray%2!=0))
+console.log(useFilterMethodToGetOdd)
+
+//map function
+console.log("--------------")
+let newMappedArray = useFilterMethodToGetEven.map(useFilterMethodToGetEven=> useFilterMethodToGetEven*3)
+console.log(newMappedArray)
+let sumOfArray= newMappedArray.reduce((sum,number)=>sum+number,0)         ///add all values in the array
+console.log(sumOfArray)
