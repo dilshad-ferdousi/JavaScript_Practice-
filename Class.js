e80d110914315916fdd8c2c9b2af3d8cc49804c7
dilshad-ferdousi <dilshad.tonu@gmail.com>
2022-03-25 22:04:42 +0600

    changes in class

--- Class.js
+++ Class.js
@@ -1,7 +1,25 @@
-class Person{
+module.exports = class Person
+{
     age =24
     get location() {return "Canada"}
+
+
+//Constructer is a metod which executes by default when you an object of the class 
+constructor(firstName,lastName)    //when you define a variable in a constructor it calls instance variable
+    {
+        this.firstName =firstName
+        this.lastName =lastName
+    }     
+    
+    //methods
+    fullName() {
+        this.firstName + this.lastName
+    }
+
+
+
 }
-let person = new Person()
+let person = new Person("Tim","Robins")
 console.log(person.age)
 console.log(person.location)
+console.log(person.fullName)
